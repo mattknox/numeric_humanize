4c3debabbd7b0793195d89fcbf57cc3965cd0a8c
matt knox <matthewknox@gmail.com>
2011-05-03 11:30:05 -0700

    added specs

---
+++ lib/numeric_humanize.rb
@@ -0,0 +1,18 @@
+class Numeric
+  SCALE_TO_WORD = Hash.new do |h, i|
+    " * 10^#{i * 3}"
+  end.merge({ 1  => " thousand",
+              2  => " million",
+              3  => " billion",
+              4  => " trillion"
+            })
+  # in my unscientific test, no one really found names beyond trillion useful.
+
+  def humanize(figures = 3)
+    scale = (Math.log10(self) / 3).floor
+    base = 1000 ** scale
+    suffix = SCALE_TO_WORD[scale.abs]
+    suffix = "#{suffix}ths" if scale < 1 && suffix
+    sprintf("%.#{ figures }G", self.to_f / base) + suffix.to_s
+  end
+end
--- spec/numeric_humanize_spec.rb
+++ spec/numeric_humanize_spec.rb
@@ -1,7 +1,17 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "NumericHumanize" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe "Numeric#humanize" do
+  it "humanizes" do
+    (10 ** 7).humanize.should == "10 million"
+    (10 ** 8).humanize.should == "100 million"
+    (10 ** 9).humanize.should == "1 billion"
+    (10 ** -9).humanize.should == "1 billionths"
+    (10 ** -8).humanize.should == "10 billionths"
+    9769160976976976.humanize.should == "9.77 * 10^15"
+    9769160976976976.humanize(2).should == "9.8 * 10^15"
+    9769160976976976.humanize(4).should == "9.769 * 10^15"
+    976916097697697600.humanize.should == "977 * 10^15"
+    97691609769.humanize.should == "97.7 billion"
+    9769160976.humanize.should == "9.77 billion"
   end
 end
